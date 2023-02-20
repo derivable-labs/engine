4c79fb8de0b69a21c6ea0b060aadc157cdfc6e29
khanhpham <khanhpham08121996@gmail.com>
2023-02-20 14:37:24 +0700

    history: fix leverage is wrong

--- src/services/history.ts
+++ src/services/history.ts
@@ -1,13 +1,10 @@
 // @ts-nocheck
 import {BigNumber, ethers} from "ethers";
-import {UniV2Pair} from "./uniV2Pair";
 import {bn} from "../utils/helper";
-import _, {transform} from "lodash";
+import _ from "lodash";
 import {PowerState} from 'powerLib/dist/powerLib'
 import {LogType, StatesType} from "../types";
 import {CurrentPool} from "./currentPool";
-import PoolOverride from "../abi/PoolOverride.json";
-import EventsAbi from "../abi/Events.json";
 
 type ConfigType = {
   account?: string
@@ -51,21 +48,20 @@ export class History {
 
       //@ts-ignore
       const result = []
-      const balancesToCalculateLeverage = {}
       const balances: { [key: number]: BigNumber } = {}
 
       orders.forEach((txs: LogType[]) => {
         const cAmount = bn(0)
         const cp = bn(0)
         const oldBalances = _.cloneDeep(balances)
-        const oldLeverage = p.calculateCompExposure(balancesToCalculateLeverage)
+        const oldLeverage = this.calculateLeverage(p, oldBalances, powers)
         //
         for (const tx of txs) {
           if (tx.args.name === 'Transfer') {
             const encodeData = ethers.utils.defaultAbiCoder.encode(
               ["address", "address", "uint256"], tx.args.args
             )
-            const formatedData =  ethers.utils.defaultAbiCoder.decode(
+            const formatedData = ethers.utils.defaultAbiCoder.decode(
               ["address from", "address to", "uint256 value"],
               encodeData
             )
@@ -83,7 +79,7 @@ export class History {
             const encodeData = ethers.utils.defaultAbiCoder.encode(
               ["address", "address", "address", "uint256", "uint256"], tx.args.args
             )
-            const formatedData =  ethers.utils.defaultAbiCoder.decode(
+            const formatedData = ethers.utils.defaultAbiCoder.decode(
               ["address operator", "address from", "address to", "uint256 id", "uint256 value"],
               encodeData
             )
@@ -97,19 +93,8 @@ export class History {
               balances[id] = balances[id] ? balances[id].add(formatedData.value) : bn(0).add(formatedData.value)
             }
           }
-
-          // balances[step.idIn.toString()] = balances[step.idIn.toString()] ? balances[step.idIn.toString()].sub(step.amountIn) : bn(0).sub(step.amountIn)
-          // balances[step.idOut.toString()] = balances[step.idOut.toString()] ? balances[step.idOut.toString()].add(step.amountOutMin) : bn(0).add(step.amountOutMin)
-          //
-          // if (powers[step.idIn]) {
-          //   balancesToCalculateLeverage[powers[step.idIn]] = balancesToCalculateLeverage[powers[step.idIn]] ? balancesToCalculateLeverage[powers[step.idIn]].sub(step.amountIn) : bn(0).sub(step.amountIn)
-          // }
-          // if (powers[step.idOut]) {
-          //   balancesToCalculateLeverage[powers[step.idOut]] = balancesToCalculateLeverage[powers[step.idOut]] ? balancesToCalculateLeverage[powers[step.idOut]].add(step.amountOutMin) : bn(0).add(step.amountOutMin)
-          // }
         }
 
-        // const newLeverage = p.calculateCompExposure(balancesToCalculateLeverage)
         const newLeverage = this.calculateLeverage(p, balances, powers)
 
         result.push({
@@ -125,38 +110,6 @@ export class History {
         })
       })
 
-      // for (const swapLog of swapLogs) {
-      //   if (swapLog.args.pool !== poolAddress) continue
-      //
-      //   const steps = swapLog.args.steps
-      //   const cAmount = bn(0)
-      //   const cp = bn(0)
-      //   const oldBalances = _.cloneDeep(balances)
-      //   const oldLeverage = p.calculateCompExposure(balancesToCalculateLeverage)
-      //
-      //   for (const step of steps) {
-      //     balances[step.idIn.toString()] = balances[step.idIn.toString()] ? balances[step.idIn.toString()].sub(step.amountIn) : bn(0).sub(step.amountIn)
-      //     balances[step.idOut.toString()] = balances[step.idOut.toString()] ? balances[step.idOut.toString()].add(step.amountOutMin) : bn(0).add(step.amountOutMin)
-      //
-      //     if (powers[step.idIn]) {
-      //       balancesToCalculateLeverage[powers[step.idIn]] = balancesToCalculateLeverage[powers[step.idIn]] ? balancesToCalculateLeverage[powers[step.idIn]].sub(step.amountIn) : bn(0).sub(step.amountIn)
-      //     }
-      //     if (powers[step.idOut]) {
-      //       balancesToCalculateLeverage[powers[step.idOut]] = balancesToCalculateLeverage[powers[step.idOut]] ? balancesToCalculateLeverage[powers[step.idOut]].add(step.amountOutMin) : bn(0).add(step.amountOutMin)
-      //     }
-      //   }
-      //   const newLeverage = p.calculateCompExposure(balancesToCalculateLeverage)
-      //   result.push({
-      //     transactionHash: swapLog.transactionHash,
-      //     timeStamp: swapLog.timeStamp,
-      //     cp,
-      //     oldBalances,
-      //     newBalances: _.cloneDeep(balances),
-      //     cAmount,
-      //     newLeverage,
-      //     oldLeverage
-      //   })
-      // }
       //@ts-ignore
       return result.sort((a, b) => (b.blockNumber - a.blockNumber))
     } catch (e) {
@@ -169,7 +122,7 @@ export class History {
     const _balances = {}
     for (let i in balances) {
       if (powers[i]) {
-        _balances[i] = balances[i]
+        _balances[powers[i]] = balances[i]
       }
     }
     return powerState.calculateCompExposure(_balances)
