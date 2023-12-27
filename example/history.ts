0b467aa2871c9def2751fd0fc91720e5c741505f
Zergity <zergity@gmail.com>
2023-12-27 15:41:18 +0700

    interceptor

--- example/history.ts
+++ example/history.ts
@@ -1,9 +1,80 @@
 import { Engine } from '../src/engine'
-import { ethers } from 'ethers'
 import { getTestConfigs } from './shared/testConfigs'
 
 const chainId = Number(process.env.CHAIN ?? 42161)
 const wallet = process.env.WALLET ?? '0xE61383556642AF1Bd7c5756b13f19A63Dc8601df'
+
+const objHash = require('object-hash');
+
+async function calcRequestID(request: any): Promise<string> {
+  // console.log(request)
+  if (request.method === 'POST') {
+    const body = await request.clone().json()
+    delete body.id
+    return objHash({
+      url: request.url,
+      body: body,
+    })
+  }
+  return objHash(request.url)
+}
+
+const fs = require('fs')
+
+import { ClientRequestInterceptor } from '@mswjs/interceptors/ClientRequest'
+const interceptor = new ClientRequestInterceptor()
+
+// Enable the interception of requests.
+interceptor.apply()
+
+// Listen to any "http.ClientRequest" being dispatched,
+// and log its method and full URL.
+interceptor.on('request', async ({ request }) => {
+  const id = await calcRequestID(request)
+  console.log(id)
+  try {
+    const json = await fs.readFileSync(`example/data/${id}.json`, 'utf8')
+    if (json) {
+      console.log(json)
+      // const res = JSON.parse(json)
+      // console.log(res)
+      // request.respondWith(new Response(res))
+    }
+  } catch (err) {
+  }
+})
+
+// Listen to any responses sent to "http.ClientRequest".
+// Note that this listener is read-only and cannot affect responses.
+interceptor.on('response', async ({ response, isMockedResponse, request }) => {
+  if (isMockedResponse) {
+    return
+  }
+  const id = await calcRequestID(request)
+  const responseClone: Response = response.clone()
+  const res = {
+    type: responseClone.type,
+    status: responseClone.status,
+    statusText: responseClone.statusText,
+    // headers: responseClone.headers,
+    // headers: Object.fromEntries(responseClone.headers.entries()),
+  }
+  try {
+    // @ts-ignore
+    res.headers = Object.fromEntries(responseClone.headers.entries())
+  } catch (err) {
+  }
+  try {
+    const body = await responseClone.json()
+    delete body.id
+    // @ts-ignore
+    res.body = body
+  } catch (err) {
+  }
+  console.log('response', id, res)
+
+  await fs.writeFileSync(`example/data/${id}.json`, JSON.stringify(res))
+})
 
 const testLocal = async () => {
   const configs = getTestConfigs(chainId)
